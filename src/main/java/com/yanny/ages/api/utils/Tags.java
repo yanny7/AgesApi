cf6e5c43b14d8e86967f7d84d5dc012a8034b9e4
Jan Lalis <lalis.jan@gmail.com>
2020-02-05 17:40:54 +0100

    Using tags for common items

---
+++ src/main/java/com/yanny/ages/api/utils/Tags.java
@@ -0,0 +1,18 @@
+package com.yanny.ages.api.utils;
+
+import com.yanny.ages.api.Reference;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.ResourceLocation;
+
+public class Tags {
+    public static class Blocks {
+
+    }
+
+    public static class Items {
+        public static final ItemTags.Wrapper AXES = new ItemTags.Wrapper(new ResourceLocation(Reference.MODID, "axes"));
+        public static final ItemTags.Wrapper HAMMERS = new ItemTags.Wrapper(new ResourceLocation(Reference.MODID, "hammers"));
+        public static final ItemTags.Wrapper KNIFES = new ItemTags.Wrapper(new ResourceLocation(Reference.MODID, "knifes"));
+        public static final ItemTags.Wrapper FISHING_NET_MESHES = new ItemTags.Wrapper(new ResourceLocation(Reference.MODID, "fishing_net_meshes"));
+    }
+}
