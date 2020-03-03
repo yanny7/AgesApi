5ab7a626b07128778b74d108c23302aafd1abb0b
Jan Lalis <lalis.jan@gmail.com>
2020-03-03 16:06:41 +0100

    Removed shears tag resource replacement by injection

--- src/main/java/com/yanny/ages/api/utils/Tags.java
+++ src/main/java/com/yanny/ages/api/utils/Tags.java
@@ -14,5 +14,6 @@ public class Tags {
         public static final ItemTags.Wrapper HAMMERS = new ItemTags.Wrapper(new ResourceLocation(Reference.MODID, "hammers"));
         public static final ItemTags.Wrapper KNIFES = new ItemTags.Wrapper(new ResourceLocation(Reference.MODID, "knifes"));
         public static final ItemTags.Wrapper FISHING_NET_MESHES = new ItemTags.Wrapper(new ResourceLocation(Reference.MODID, "fishing_net_meshes"));
+        public static final ItemTags.Wrapper SHEARS = new ItemTags.Wrapper(new ResourceLocation("forge", "shears"));
     }
 }
